f05dc22770cba346f94d9d0728909a9c8b306bf2
Anatolii.Zamlynnyi <Anatolii.Zamlynnyi@gmail.com>
2020-04-04 16:55:03 +0300

    create react object

--- AppReact/ClientApp/src/components/Test.js
+++ AppReact/ClientApp/src/components/Test.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
-    state = {  }
+    state = { 
+        animals: [
+            {
+                id: 1,
+                name: "Носоріг",
+                image: "https://ichef.bbci.co.uk/images/ic/720x405/p061q6g2.jpg",
+                age: 5
+            },
+            {
+                id: 2,
+                name: "Носоріг2",
+                image: "https://ichef.bbci.co.uk/images/ic/720x405/p061q6g2.jpg",
+                age: 5
+            }
+        ]
+     }
     render() { 
+        const content=this.state.animals.map(
+            (animal)=>{
+                return <h1 key={animal.id}>{animal.name}</h1>            
+            }
+        );
+
         return ( <div>
-            Hello test!
+            {content}
         </div> );
     }
 }
