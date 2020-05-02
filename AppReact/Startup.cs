ee765ff88f30c790bf34da25bc5dab2438a2dd10
novakvova <novakvova@gmail.com>
2020-05-02 15:56:51 +0300

    Add bag database

--- AppReact/Startup.cs
+++ AppReact/Startup.cs
@@ -77,8 +77,10 @@ namespace AppReact
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            ApplicationDbContext context)
         {
+            context.Database.EnsureCreated();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
