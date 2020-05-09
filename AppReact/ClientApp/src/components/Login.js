3b2e5c242e4bcdc87537d7e32a9eccce43f075d1
Anatolii.Zamlynnyi <Anatolii.Zamlynnyi@gmail.com>
2020-05-09 13:05:46 +0300

    edit form

--- AppReact/ClientApp/src/components/Login.js
+++ AppReact/ClientApp/src/components/Login.js
@@ -8,21 +8,21 @@ const Login = props => (
                 <form asp-action="Login">
                     <div asp-validation-summary="ModelOnly" className="text-danger"></div>
                     <div className="form-group">
-                        <label asp-for="Email" className="control-label"></label>
+                        <label asp-for="Email" className="control-label">E-mail:</label>
                         <input asp-for="Email" className="form-control" />
                         <span asp-validation-for="Email" className="text-danger"></span>
                     </div>
                     <div className="form-group">
-                        <label asp-for="Password" className="control-label"></label>
+                        <label asp-for="Password" className="control-label">Password:</label>
                         <input asp-for="Password" className="form-control" />
                         <span asp-validation-for="Password" className="text-danger"></span>
                     </div>
                     <div>
                         <input type="checkbox" asp-for="RememberMe" />
-                        <label asp-for="RememberMe"></label><br />
+                        <label asp-for="RememberMe">Remember me!</label><br />
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Вхід" className="btn btn-success" />
+                        <input type="submit" value="Login" className="btn btn-success" />
                     </div>
                 </form>
             </div>
--- AppReact/ClientApp/src/components/Registration.js
+++ AppReact/ClientApp/src/components/Registration.js
@@ -8,27 +8,27 @@ const Registration = props => (
                 <form asp-action="Register">
                     <div asp-validation-summary="ModelOnly" className="text-danger"></div>
                     <div className="form-group">
-                        <label asp-for="Phone" className="control-label"></label>
+                        <label asp-for="Phone" className="control-label">Phone:</label>
                         <input asp-for="Phone" className="form-control" />
                         <span asp-validation-for="Phone" className="text-danger"></span>
                     </div>
                     <div className="form-group">
-                        <label asp-for="Email" className="control-label"></label>
+                        <label asp-for="Email" className="control-label">E-mail:</label>
                         <input asp-for="Email" className="form-control" />
                         <span asp-validation-for="Email" className="text-danger"></span>
                     </div>
                     <div className="form-group">
-                        <label asp-for="Password" className="control-label"></label>
+                        <label asp-for="Password" className="control-label">Password:</label>
                         <input asp-for="Password" className="form-control" />
                         <span asp-validation-for="Password" className="text-danger"></span>
                     </div>
                     <div className="form-group">
-                        <label asp-for="PasswordConfirm" className="control-label"></label>
+                        <label asp-for="PasswordConfirm" className="control-label">Confirm password:</label>
                         <input asp-for="PasswordConfirm" className="form-control" />
                         <span asp-validation-for="PasswordConfirm" className="text-danger"></span>
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Реєстрація" className="btn btn-success" />
+                        <input type="submit" value="Registration" className="btn btn-success" />
                     </div>
                 </form>
             </div>
